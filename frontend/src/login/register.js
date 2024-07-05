1efec07d668277ac8e787e4622295d2d1d1f8c2b
shaikabdulkhaderjilani <shaikabdulkhaderjilani0@gmail.com>
2024-07-05 14:29:29 +0530

    first commit

--- frontend/src/login/register.js
+++ frontend/src/login/register.js
@@ -1,86 +1,36 @@
 // import React from 'react';
-// import {
-//   MDBBtn,
-//   MDBContainer,
-//   MDBRow,
-//   MDBCol,
-//   MDBCard,
-//   MDBCardBody,
-//   MDBInput,
-//   MDBSelect,
-//   MDBRadio
-// }
-// from 'mdb-react-ui-kit';
-
-// function Register() {
-//   return (
-//     <MDBContainer fluid>
-
-//       <MDBRow className='justify-content-center align-items-center m-5'>
-
-//         <MDBCard>
-//           <MDBCardBody className='px-4'>
-
-//             <h3 className="fw-bold mb-4 pb-2 pb-md-0 mb-md-5">Registration Form</h3>
-
-//             <MDBRow>
-
-//               <MDBCol md='6'>
-//                 <MDBInput wrapperClass='mb-4' label='First Name' size='lg' id='form1' type='text'/>
-//               </MDBCol>
-
-//               <MDBCol md='6'>
-//                 <MDBInput wrapperClass='mb-4' label='Last Name' size='lg' id='form2' type='text'/>
-//               </MDBCol>
-
-//             </MDBRow>
-
-//             <MDBRow>
-
-//               <MDBCol md='6'>
-//                 <MDBInput wrapperClass='mb-4' label='Birthday' size='lg' id='form3' type='text'/>
-//               </MDBCol>
-
-//               <MDBCol md='6' className='mb-4'>
-//                 <h6 className="fw-bold">Gender: </h6>
-//                 <MDBRadio name='inlineRadio' id='inlineRadio1' value='option1' label='Female' inline />
-//                 <MDBRadio name='inlineRadio' id='inlineRadio2' value='option2' label='Male' inline />
-//                 <MDBRadio name='inlineRadio' id='inlineRadio3' value='option3' label='Other' inline />
-//               </MDBCol>
-
-//             </MDBRow>
-
-//             <MDBRow>
-
-//               <MDBCol md='6'>
-//                 <MDBInput wrapperClass='mb-4' label='Email' size='lg' id='form4' type='email'/>
-//               </MDBCol>
-
-//               <MDBCol md='6'>
-//                 <MDBInput wrapperClass='mb-4' label='Phone Number' size='lg' id='form5' type='rel'/>
-//               </MDBCol>
-
-//             </MDBRow>
-
-//             <MDBSelect
-//               label='Choose option'
-//               className='mb-4'
-//               size='lg'
-//               data={[
-//                 { text: 'Choose option', value: 1, disabled: true },
-//                 { text: 'Subject 1', value: 2 },
-//                 { text: 'Subject 2', value: 3 },
-//                 { text: 'Subject 3', value: 4 }
-//               ]}
-//               />
-//             <MDBBtn className='mb-4' size='lg'>Submit</MDBBtn>
-
-//           </MDBCardBody>
-//         </MDBCard>
-
-//       </MDBRow>
-//     </MDBContainer>
-//   );
-// }
-
-// export default Register;
+import Naav from '../home/navbar';
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBCheckbox
+  }
+  from 'mdb-react-ui-kit';
+  
+  function App() {
+    return (
+        <><Naav />
+      <MDBContainer fluid className='d-flex align-items-center justify-content-center bg-image' style={{backgroundImage: 'url()'}}>
+        <div className='mask gradient-custom-3'></div>
+        <MDBCard className='m-5' style={{maxWidth: '600px'}}>
+          <MDBCardBody className='px-5'>
+            <h2 className="text-uppercase text-center mb-5">Create an account</h2>
+            <MDBInput wrapperClass='mb-4' label='Your Name' size='lg' id='form1' type='text'/>
+            <MDBInput wrapperClass='mb-4' label='Your Email' size='lg' id='form2' type='email'/>
+            <MDBInput wrapperClass='mb-4' label='Password' size='lg' id='form3' type='password'/>
+            <MDBInput wrapperClass='mb-4' label='Repeat your password' size='lg' id='form4' type='password'/>
+            <div className='d-flex flex-row justify-content-center mb-4'>
+              <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I agree all statements in Terms of service' />
+            </div>
+            <MDBBtn className='mb-4 w-100 gradient-custom-4' size='lg'>Register</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBContainer>
+      </>
+    );
+  }
+  
+  export default App;
